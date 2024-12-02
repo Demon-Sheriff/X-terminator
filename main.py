6ac12115d13ccb5a28bc88d34d7b4dc99abe79ec
Anant Shukla <anantshukla404@gmail.com>
2024-12-02 22:16:30 +0530

    testing the bearer_token

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import tweepy.client
 
 load_dotenv()
 
@@ -7,8 +8,9 @@ api_key = os.getenv("API_KEY")
 api_secret = os.getenv("API_SECRET")
 access_key = os.getenv("ACCESS_TOKEN")
 access_secret = os.getenv("ACCESS_SECRET")
+bearer_token = os.getenv("BEARER_TOKEN")
 
-print(f"api key : {api_key}, api secret : {api_secret}, access_key : {access_key}, access_secret : {access_secret}") 
+print(f"api key : {api_key}, api secret : {api_secret}, access_key : {access_key}, access_secret : {access_secret}, bearer_token : {bearer_token}") 
 
 import tweepy 
 
@@ -25,14 +27,18 @@ import tweepy
 # for tweet in tweets:
 #     print(tweet.text, '\n')
 
-auth = tweepy.OAuthHandler(api_key, api_secret)
-auth.set_access_token(access_key, access_secret)
+# auth = tweepy.OAuthHandler(api_key, api_secret)
+# auth.set_access_token(access_key, access_secret)
 
-api = tweepy.API(auth)
+# api = tweepy.API(auth)
+# user = api.get_user(screen_name="Twitter")
+# client = tweepy.client(bearer_token=)
 
-followers = api.get_followers()
+# followers = api.get_followers()
+
+# print(user.screen_name)
 
 # print(followers)
 
-for follower in followers:
-    print(f"{follower.screen_name}")
+# for follower in followers:
+#     print(f"{follower.screen_name}")
