260faf4d1202ddb61863effefff0082657aa9162
Anant Shukla <anantshukla404@gmail.com>
2024-12-03 12:15:40 +0530

    thank you elon for not even letting me access my own followers list using the free X api, have to switch to scraping solutions

--- main.py
+++ main.py
@@ -14,31 +14,41 @@ print(f"api key : {api_key}, api secret : {api_secret}, access_key : {access_key
 
 import tweepy 
 
-# auth = tweepy.OAuth1UserHandler(
-#     api_key,
-#     api_secret,
-#     access_key,
-#     access_secret
-# )
+auth = tweepy.OAuth1UserHandler(
+    api_key,
+    api_secret,
+    access_key,
+    access_secret
+)
 
-# api = tweepy.API(auth)
 
 # tweets = api.home_timeline(count=4)
 # for tweet in tweets:
 #     print(tweet.text, '\n')
 
-# auth = tweepy.OAuthHandler(api_key, api_secret)
-# auth.set_access_token(access_key, access_secret)
+auth = tweepy.OAuthHandler(api_key, api_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
+screen_name="prompt_Tunes"
+
+followers = api.get_followers(screen_name=screen_name, count=100)
+for follower in followers:
+    print(follower.screen_name)
+# user = api.get_user(screen_name=screen_name)
+# user_id = user.id_str
+
+# print(user_id)
 
 # api = tweepy.API(auth)
 # user = api.get_user(screen_name="Twitter")
-# client = tweepy.client(bearer_token=)
+print('rate limit exceeds here')
+# client = tweepy.Client(bearer_token=bearer_token, consumer_key=api_key, consumer_secret=api_secret, access_token=access_key, access_token_secret=access_secret, wait_on_rate_limit=True)
 
-# followers = api.get_followers()
+# print('here')
+# user_id = client.get_user(username='prompt_Tunes').data.id
+# # print(user_id)
+# print('or maybe here')
 
-# print(user.screen_name)
+# followers = client.get_users_followers(user_id,)
 
-# print(followers)
-
-# for follower in followers:
-#     print(f"{follower.screen_name}")
+# followers = client.get_users_followers('prompt_Tunes')
