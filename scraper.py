66cf6383b74de290d1ebf215847dc8de2ebf69d1
Anant Shukla <anantshukla404@gmail.com>
2024-12-05 22:46:56 +0530

    testing the scraper

--- scraper.py
+++ scraper.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import os, dotenv
 
@@ -32,6 +34,12 @@ password = driver.find_element("xpath", "//input[@autocomplete='current-password
 password.send_keys(os.getenv("PASSWORD"))
 password.send_keys(Keys.RETURN)
 time.sleep(4)
+
+# wait for login completion (max wait is 5 seconds)
+time.sleep(4) # wait for 4 seconds (to update this one.)
+# WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CLASS_NAME, "css-175oi2r r-6koalj r-eqz5dr r-16y2uox r-1habvwh r-cnw61z r-13qz1uu r-1ny4l3l r-1loqt21")))
+# after logging in navigate to the profile section of the user.
+
 
 print("done")
 
